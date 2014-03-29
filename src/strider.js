3c4b1b0c2fda0eb825abec76e43c251c778eca17
Andrew Blick <andrew@blicksky.com>
2014-03-29 01:52:56 -0400

    added initial functionality and tests

--- src/strider.js
+++ src/strider.js
@@ -1,5 +1,33 @@
 YUI.add('strider', function(Y) {
 	
+	var Strider = function(config) {
+		Strider.superclass.constructor.apply(this, arguments);
+	}
 	
+	Strider.NAME = "strider";
 	
+	Strider.ATTRS = {
+		striderNode: {
+			readOnly: true
+		},
+		
+		contextNode: {
+			readOnly: true
+		}
+	};
+	
+	Y.extend(Strider, Y.Base, {
+		initializer: function(config) {
+			this._set('striderNode', Y.one(config.striderNode));
+			this._set('contextNode', Y.one(config.contextNode));
+		},
+		
+		destructor: function() {
+		}
+	});
+	
+	Y.Strider = Strider;
+	
+}, '0.0.1', {
+	requires: ['base']
 });
